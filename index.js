226abcf125f2c12029d8469053e25ea6c84c782a
Ian Cossentino <icossentino@gmail.com>
2021-02-24 10:27:17 -0500

    commit tree f747c49670f273b30b440a0a81b2d64ff8bbadfb

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, val) {
+  // return new driver with updated value for the key
+  let newObj = Object.assign({}, driver);
+  newObj[key] = val;
+  return newObj;
+}
+
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, val) {
+  // return edited version of original driver with updated value for the key
+  driver[key] = val;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  // return new obj with deleted key/value pair by key entered as arg
+  let newObj = Object.assign({}, driver);
+  delete newObj[key];
+  return newObj;
+}
+
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  // delete key/value pair by key entered as arg from original obj
+  delete driver[key];
+  return driver;
+}
